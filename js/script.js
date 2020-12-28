86158c778a3e25574e1a8660034902f9a1ed1a2e
jkhamanishi <jkhamanishi@gmail.com>
2020-12-27 17:37:17 -0800

    Backup Testing

--- js/script.js
+++ js/script.js
@@ -7,48 +7,66 @@ function hello() {
     alert('Hello world! (test successful)');
 };
 
+// var gridData = flexA;
 
-/* Grid Scrips */
-var gridData = flexA;
-console.log(gridData); // var gridData from gridData.js
+//window.onload = loadGrid(flexA);
+$(document).ready(function(){loadGrid(flexA);});
+//$(document).ready(loadGrid(flexA));
 
 
-window.onload = function(){
 
-var grid = document.getElementById("grid");
+// --- Grid Scripts ---
+// --------------------
 
-grid.style.setProperty('grid-template-columns', "1fr ".repeat(gridData[0].length));
+console.log(flexA); // var gridData from gridData.js
 
-for (var i=0; i<gridData.length; i++){ // for every row
-    
-    for (var j=0; j<gridData[0].length; j++){ // for every column
-        var newDIV = document.createElement("DIV");
-        //newDIV.innerHTML = "corn";
-        newDIV.classList.add('empty');
-        newDIV.setAttribute("ondragover", "allowDrop(event)");
-        newDIV.setAttribute("ondrop", "drop(event)");
-        //newDIV.setAttribute("ondragenter", "dragEnter()");
-        //newDIV.setAttribute("dragleave", "dragLeave()");
-        newDIV.addEventListener("dragenter", dragEnter);
-        newDIV.addEventListener("dragleave", dragLeave);
-        grid.appendChild(newDIV);
+function loadGrid(gridData){
+//window.onload = function(){
+    var grid = document.getElementById("grid");
+    grid.innerHTML = "";
+
+    grid.style.setProperty('grid-template-columns', "1fr ".repeat(gridData[0].length));
+
+    for (var i=0; i<gridData.length; i++){ // for every row
         
-        if (gridData[i][j] != "") {
-            var dragDIV = document.createElement("DIV");
-            dragDIV.innerHTML = gridData[i][j];
-            //dragDIV.setAttribute("style", "width: 100px; height: 100px;");
-            //dragDIV.height = "100px";
-            dragDIV.classList.add('drag');
-            dragDIV.setAttribute("draggable", "true");
-            dragDIV.setAttribute("ondragstart", "drag(event)");
-            dragDIV.id = gridData[i][j].replace(/ +/g, "");
-            dragDIV.style.setProperty("cursor", "move")
-            newDIV.appendChild(dragDIV);
+        for (var j=0; j<gridData[0].length; j++){ // for every column
+            var newDIV = document.createElement("DIV");
+            //newDIV.innerHTML = "corn";
+            newDIV.classList.add('empty');
+            newDIV.setAttribute("ondragover", "allowDrop(event)");
+            newDIV.setAttribute("ondrop", "drop(event)");
+            //newDIV.setAttribute("ondragenter", "dragEnter()");
+            //newDIV.setAttribute("dragleave", "dragLeave()");
+            newDIV.addEventListener("dragenter", dragEnter);
+            newDIV.addEventListener("dragleave", dragLeave);
+            grid.appendChild(newDIV);
+            
+            if (gridData[i][j] != "") {
+                var dragDIV = document.createElement("DIV");
+                dragDIV.innerHTML = gridData[i][j];
+                //dragDIV.setAttribute("style", "width: 100px; height: 100px;");
+                //dragDIV.height = "100px";
+                dragDIV.classList.add('drag');
+                dragDIV.setAttribute("draggable", "true");
+                dragDIV.setAttribute("ondragstart", "drag(event)");
+                dragDIV.id = gridData[i][j].replace(/ +/g, "");
+                dragDIV.style.setProperty("cursor", "move")
+                newDIV.appendChild(dragDIV);
+            }
         }
+        
     }
-    
 }
-}
+
+
+
+
+
+
+
+
+// --- Drag 'n Drog functions ---
+// ------------------------------
 
 function allowDrop(ev) {
   if (ev.target.innerHTML == "") {ev.preventDefault();}
@@ -71,6 +89,10 @@ function dragLeave() {
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
+
+
+
+
 
 
 
