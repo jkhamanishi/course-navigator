10990bb203bf561f91ea9b7f887225cfa2d60920
jkhamanishi <jkhamanishi@gmail.com>
2020-12-28 18:20:56 -0800

    Update main files: added show/hide feature

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ function hello() {
     alert('Hello world! (test successful)');
 };
 
-
 function log(text){console.log(text)};
+
 
 
 // --- Grid Scripts ---
@@ -46,8 +46,10 @@ function loadGrid(gridData){
                 dragDIV.setAttribute("ondragstart", "dragStart(event)");
                 dragDIV.setAttribute("ondrag", "dragging(event)");
                 dragDIV.setAttribute("ondragend", "dragEnd()");
+                dragDIV.setAttribute("onclick", "spotlightCourse(event)");
                 dragDIV.id = courseCode.replace(/ +/g, "");
                 dragDIV.style.setProperty("cursor", "move")
+                dragDIV.style.display = "initial";
                 newDIV.appendChild(dragDIV);
                 //dragElement(dragDIV);
                 
@@ -65,11 +67,11 @@ function loadGrid(gridData){
                         svg.innerHTML += `
                             <svg>
                             <defs>
-                              <marker id="arrowhead" markerWidth="13" markerHeight="13" refx="6" refy="3" orient="auto">
+                              <marker id="arrowhead`+prereq+"-"+courseCode+`" markerWidth="13" markerHeight="13" refx="6" refy="3" orient="auto">
                                 <polygon points="0,0 0,6 5,3" style="fill:red;" />
                               </marker>
                             </defs>
-                            <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2; marker-end: url(#arrowhead);" />
+                            <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2; marker-end: url(#arrowhead`+prereq+"-"+courseCode+`);" />
                             </svg>`
                        //var svg = document.getElementsByClassName("svgContainer")[1];
                        document.getElementById("allSVGs").appendChild(svg);
@@ -119,6 +121,8 @@ function dragLeave() {
 
 function dragStart(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    var placeholders = document.getElementsByClassName("empty");
+    for (x of placeholders) {x.style.outline = "0.1em dotted grey";}
 }
 function dragging(ev) {
     draggedCourse = ev.target.id;
@@ -126,12 +130,22 @@ function dragging(ev) {
         svg.setAttribute("obj2", newlocation)
         orientArrow(svg);
         svg.setAttribute("obj2", draggedCourse);
+    }
+    for (svg of findArrows("obj1", ev.target.id)) {
+        svg.setAttribute("obj1", newlocation)
+        orientArrow(svg);
+        svg.setAttribute("obj1", draggedCourse);
 }   }
 function dragEnd() {
     orientAllArrows();
     var placeholders = document.getElementsByClassName("empty");
-    for (x of placeholders) {x.style.backgroundColor = "";};
-}
+    for (x of placeholders) {
+        x.style.backgroundColor = ""; 
+        x.style.outline = "";
+}   }
+
+
+
 
 
 
@@ -140,10 +154,10 @@ function dragEnd() {
 
 
 // --- Moving the Arrows ---
-// ----------------------------------
+// -------------------------
 
 
-function orientArrow(svg, obj1, obj2) {
+function orientArrow(svg) {
     var arrow = svg.getElementsByTagName("line")[0];
     
     var obj1 = document.getElementById(svg.getAttribute("obj1")).getBoundingClientRect();
@@ -165,9 +179,10 @@ function orientArrow(svg, obj1, obj2) {
     svg.style.setProperty('left', Math.min(obj1.left, obj2.left)+"px");
     svg.style.setProperty('top', Math.min(obj1.top, obj2.top)+"px");
     var svgPos = svg.firstElementChild.getBoundingClientRect();
-    svg.firstElementChild.setAttribute('width', Math.max(obj1.right, obj2.right)-svgPos.left)
-    svg.firstElementChild.setAttribute('height', Math.max(obj1.bottom, obj2.bottom)-svgPos.top)
-
+    svg.firstElementChild.setAttribute('width', Math.abs(Math.max(obj1.right, obj2.right)-svgPos.left));
+    svg.firstElementChild.setAttribute('height', Math.abs(Math.max(obj1.bottom, obj2.bottom)-svgPos.top));
+    
+    
     //Prioritize connecting the arrow to the top and bottom of the boxes
     var verticlePriority = true;
     
@@ -194,10 +209,17 @@ function orientArrow(svg, obj1, obj2) {
     }
 }
 
-function orientAllArrows() {
-    var allSVGs = document.getElementsByClassName("svgContainer");
-    for (svg of allSVGs) {
-        orientArrow(svg);
+function orientAllArrows(length = 1) {
+    for (var i=0; i < length; i++) {
+        var allSVGs = document.getElementsByClassName("svgContainer");
+        for (svg of allSVGs) {
+            orientArrow(svg);
+            if (coursesVisible(svg)) {
+                svg.style.display = "initial";
+            } else {
+                svg.style.display = "none";
+            }
+        }
     }
 }
 
@@ -209,6 +231,87 @@ function findArrows(attribute, value) {
     }
     return result;
 }
+
+function coursesVisible(svg) {
+    var obj1_id = svg.getAttribute("obj1");
+    var obj1_status = document.getElementById(obj1_id).style.display;
+    var obj1_visible = false;
+    if (obj1_status !== "none") {obj1_visible = true}
+    
+    var obj2_id = svg.getAttribute("obj2");
+    var obj2_status = document.getElementById(obj2_id).style.display;
+    var obj2_visible = false;
+    if (obj2_status !== "none") {obj2_visible = true}
+    
+    // if ((obj1_visible && obj2_visible)) {log(obj1_status + ", " + obj2_status)}
+    
+    return (obj1_visible && obj2_visible)
+}
+
+
+
+
+
+
+
+// --- Show/Hide Related Courses and Arrows---
+// ----------------------------------
+
+var lastclicked;
+
+var allCourses = document.getElementsByClassName("drag");
+
+function spotlightCourse(ev) {
+    var course = ev.target;
+    if (lastclicked !== course.id){
+        lastclicked = course.id;
+        for (x of allCourses) {
+            x.style.display = "none";
+            x.style.setProperty("background-color", "lightblue");
+        }
+        course.style.setProperty("background-color", "yellow");
+        showPrereq(course);
+        showLeadingTo(course);
+        orientAllArrows(3);
+    } else {
+        for (x of allCourses) {
+            x.style.display = "initial";
+            x.style.setProperty("background-color", "lightblue");
+        }
+        orientAllArrows();
+        lastclicked = "";
+        orientAllArrows(2); 
+    }
+}
+
+function showPrereq(course) {
+    for (x of allCourses) {
+        if (x == course) {
+            x.style.display = "initial";
+        } else if (courseData[course.id].prereqs.includes(x.id)){
+            x.style.display = "initial";
+            showPrereq(x);
+        }
+    }
+}
+function showLeadingTo(course) {
+    for (x of allCourses) {
+        if (x == course) {
+            x.style.display = "initial";
+        } else if (courseData[x.id].prereqs.includes(course.id)){
+            x.style.display = "initial";
+            showLeadingTo(x);
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
